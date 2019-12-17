6720e6069f4b9009a1fa0cb07b630498c547490c
CyFx <cyfx@Admins-MacBook-Pro-2.local>
2019-12-16 22:28:20 -0600

    Gatsby Setup & Contentful API's

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HomePage from "../components/home-page"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <HomePage/>
+  </Layout>
+)
+
+export default IndexPage
