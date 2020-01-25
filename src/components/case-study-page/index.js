adcc066549fc21dbbd201cba208b21e90d43d12f
cy-fx <alexmiranda.code@gmail.com>
2020-01-25 13:56:04 -0800

    Add Basic CSS

--- src/components/case-study-page/index.js
+++ src/components/case-study-page/index.js
@@ -1,56 +1,83 @@
 import React from "react"
-import { Link } from "gatsby"
-import Img from "gatsby-image"
+import caseStudyPageStyle from "./style.module.scss"
+import LeadIn from "./lead-in"
 
 const CaseStudyPage = ({ caseStudyData }) => {
-
+  console.log(caseStudyData)
   return (
-    <div>
-      <section>
-        <div>
-          <h3>{caseStudyData.jumbotron.clientName}</h3>
-          <p>{caseStudyData.jumbotron.headline}</p>
-        </div>
-        <Img fluid={caseStudyData.jumbotron.heroImage.fluid} />
-      </section>
-      <section>
-        <div>
-          <h3>{caseStudyData.services.listName}</h3>
-          <ul>
-            {caseStudyData.services.items.map(item => (
-              <li>{item.name}</li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3>{caseStudyData.projectOverview.title}</h3>
-          <p>{caseStudyData.projectOverview.paragraph}</p>
-        </div>
-      </section>
-      <section>
-        <Img fluid={caseStudyData.fullWidthImage1.fluid} />
-        <Img fluid={caseStudyData.fullWidthImage2.fluid} />
-        <div>
-          <div>
-            <h3>{caseStudyData.projectOverview.title}</h3>
-            <p>{caseStudyData.projectOverview.paragraph}</p>
+    <div className={caseStudyPageStyle.backgroundContainer}>
+      <div className={caseStudyPageStyle.componentContainer}>
+        <section className={caseStudyPageStyle.greeting}>
+          <div className={caseStudyPageStyle.winWinContainer}>
+            <p className={caseStudyPageStyle.title}>
+              {caseStudyData.jumbotron.clientName}
+            </p>
           </div>
-          <Img fluid={caseStudyData.leadInImage.fluid} />
-        </div>
-        <Img fluid={caseStudyData.halfWidthImageLeft.fluid} />
-        <Img fluid={caseStudyData.halfWidthImageLeft.fluid} />
-      </section>
-      <section>
-        <p>{caseStudyData.testimonial.testimonial}</p>
-        <div>
-          <span>{caseStudyData.testimonial.personName}</span>
-          <span>{caseStudyData.testimonial.position}</span>
-        </div>
-      </section>
-      <section>
-        <p>Client Name</p>
-        <Link to>Next Project</Link>
-      </section>
+          <div className={caseStudyPageStyle.getInTouch}>
+            <p className={caseStudyPageStyle.paragraph}>
+              {caseStudyData.jumbotron.headline}
+            </p>
+          </div>
+        </section>
+        <section className={caseStudyPageStyle.projectOverview}>
+          <div className={caseStudyPageStyle.winWinContainer}>
+            <p className={caseStudyPageStyle.title}>
+              {caseStudyData.services.listName}
+            </p>
+            {caseStudyData.services.items.map(item => (
+              <div className={caseStudyPageStyle.services} key={item.id}>
+                {item.text}
+              </div>
+            ))}
+          </div>
+          <div className={caseStudyPageStyle.getInTouch}>
+            <p className={caseStudyPageStyle.title}>
+              {caseStudyData.projectOverview.title}
+            </p>
+            <p className={caseStudyPageStyle.paragraph}>
+              {caseStudyData.projectOverview.paragraph}
+            </p>
+          </div>
+        </section>
+        <section className={caseStudyPageStyle.showCase}>
+          <figure className={caseStudyPageStyle.fullWidthImage}></figure>
+        </section>
+        <LeadIn leadInData={caseStudyData.leadIn} />
+        <section className={caseStudyPageStyle.showCase}>
+          <figure className={caseStudyPageStyle.fullWidthImage}></figure>
+        </section>
+        <section className={caseStudyPageStyle.showCaseHalf}>
+          <figure className={caseStudyPageStyle.halfWidthImage}></figure>
+          <figure className={caseStudyPageStyle.halfWidthImage}></figure>
+        </section>
+        <section className={caseStudyPageStyle.testimonial}>
+          <div className={caseStudyPageStyle.centerBlock}>
+            <div className={caseStudyPageStyle.getInTouch}>
+              <p className={caseStudyPageStyle.testimonialParagraph}>
+                {caseStudyData.testimonial.testimonial}
+              </p>
+            </div>
+            <div className={caseStudyPageStyle.winWinContainer}>
+              <p className={caseStudyPageStyle.testimonialTitle}>
+                {caseStudyData.testimonial.personName}
+              </p>
+              <p className={caseStudyPageStyle.testimonialSubtitle}>
+                {caseStudyData.testimonial.position}
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className={caseStudyPageStyle.nextProjectBackground}>
+        <section className={caseStudyPageStyle.nextProjectContainer}>
+          <div className={caseStudyPageStyle.winWinContainer}>
+            <p className={caseStudyPageStyle.title}>Client Name</p>
+          </div>
+          <div className={caseStudyPageStyle.getInTouch}>
+            <p className={caseStudyPageStyle.paragraph}>Next Project ➝</p>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
